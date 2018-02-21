3d9649e51ac2723224bfb96e665d07de3c49e06d
Joao Luiz M. Uchoa <joao_uchoa@virgo.atlantico.net.br>
2018-02-21 10:19:09 -0300

    Update ApiContext

--- Models/ApiContext.cs
+++ Models/ApiContext.cs
@@ -4,11 +4,8 @@ namespace netcoredocker.Models
 {
     public class ApiContext : DbContext
     {
-         public ApiContext(DbContextOptions<ApiContext> options)
-            : base(options)
-        {
-            this.Database.EnsureCreated();
-        }
+        public ApiContext(DbContextOptions<ApiContext> options)
+           : base(options) => this.Database.EnsureCreated();
 
         public DbSet<Product> Products {get; set; }
     }
